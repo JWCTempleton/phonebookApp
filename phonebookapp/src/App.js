1d4a7675927c10f58b7955c55af7fee9ff6a9750
Jacob Templeton <jacob.templeton@gmail.com>
2023-02-27 15:02:59 -0800

    Add filtering system where displayed names can be filtered by names entered in search bar

--- phonebookapp/src/App.js
+++ phonebookapp/src/App.js
@@ -4,9 +4,13 @@ import { useState } from "react";
 function App() {
   const [persons, setPeople] = useState([
     { name: "Jacob Templeton", number: "925-999-9999", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchedName, setSearchedName] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -37,9 +41,18 @@ function App() {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+
+  const handleSearch = (event) => {
+    setSearchedName(event.target.value);
+  };
+
+  const filteredNames = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchedName.toLowerCase())
+  );
   return (
     <div>
       <h2>Phonebook</h2>
+      <h2>Add New Person</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -51,8 +64,12 @@ function App() {
           <button type="submit">add</button>
         </div>
       </form>
+      <h2>Person Search</h2>
+      <div>
+        name: <input value={searchedName} onChange={handleSearch} />
+      </div>
       <h2>People</h2>
-      {persons.map((person) => (
+      {filteredNames.map((person) => (
         <p key={person.id}>
           {person.name} : {person.number}
         </p>

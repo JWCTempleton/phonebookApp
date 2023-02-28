57b6bcd4ec81a0d4bfbf569252e57e6c5cfb97d1
Jacob Templeton <jacob.templeton@gmail.com>
2023-02-27 17:04:56 -0800

    Add axios, json-server, json db to populate initial person state

--- phonebookapp/src/App.js
+++ phonebookapp/src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import AllPeople from "./components/AllPeople";
 import PersonForm from "./components/PersonForm";
 import PersonSearch from "./components/PersonSearch";
 
 function App() {
-  const [persons, setPeople] = useState([
-    { name: "Jacob Templeton", number: "925-999-9999", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPeople] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchedName, setSearchedName] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPeople(response.data);
+    });
+  }, []);
 
   const addName = (event) => {
     event.preventDefault();

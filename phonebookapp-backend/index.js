44af2f3390fe9ef84958eb92ec5cf5cab5b9289d
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-03 15:15:10 -0800

    Add dotenv library, extract mongoose components to own module, utilize that module of db communication in fetching data from db and to post new person objects to the db, ensure front end is working with new changes when adding new person objects and fetching data from db

--- phonebookapp-backend/index.js
+++ phonebookapp-backend/index.js
@@ -1,54 +1,55 @@
-const { request } = require("express");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const Person = require("./models/person");
 
 app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
 
-let persons = [
-  {
-    id: 1,
-    name: "Jacob Templeton",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: 1,
+//     name: "Jacob Templeton",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((people) => {
+    response.json(people);
+  });
 });
 
 app.get("/info", (request, response) => {
-  response.send(`<div>
-  <h1>Phonebook has info for ${persons.length} people</h1>
+  Person.find({}).then((person) =>
+    response.send(`<div>
+  <h1>Phonebook has info for ${person.length} people</h1>
   <h3>${new Date()}</h3>
-  </div>`);
+  </div>`)
+  );
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -60,25 +61,24 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (
-    persons.some(
-      (person) => person.name.toLowerCase() === body.name.toLowerCase()
-    )
-  ) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
+  //   if (
+  //     persons.some(
+  //       (person) => person.name.toLowerCase() === body.name.toLowerCase()
+  //     )
+  //   ) {
+  //     return response.status(400).json({
+  //       error: "name must be unique",
+  //     });
+  //   }
 
-  const person = {
-    id: Math.floor(Math.random() * 1000000),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
